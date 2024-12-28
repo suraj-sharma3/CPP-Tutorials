de4d1245c090794dc0de5757b893783285503fb7
suraj-sharma3 <suraj.sharma13395@gmail.com>
2024-12-28 18:41:30 +0530

    test C & CPP codes added

---
+++ test_C_CPP_Codes/test.c
@@ -0,0 +1,26 @@
+// Tutorial : https://youtu.be/y-oLhL3qQGQ?si=AZ05z-cBCk9tK172
+// Tutorial Java : https://youtu.be/-0X0BHLE0Dg?si=WqNbi-T6eHrw9Rtu
+
+/* Install MinGW from sourceforge
+select g++ and base while installing MinGW
+copy the bin folder's path of MinGW and add it in the system path variable */
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!");
+    return 0;
+}
+
+/* Commands to run code : 
+
+open terminal, move to the directory that contains the code 
+compile the code with gcc by running the command : gcc file_name.c
+a new exe file would be generated for e.g. a.exe
+run the command : .\file_name (.\a) 
+
+OR
+
+just install the code runner extension in vs code 
+and run the code from the run option in VS code by clicking Run Code
+*/
